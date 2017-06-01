4b163ec66833943d1fa4949657f70fe47563b117
DK\waqar.zaidi <waqar.zaidi@valtech.com>
2017-06-01 14:29:01 +0200

    Edit action added to customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Security.AccessControl;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -11,7 +9,7 @@ namespace Vidly.Controllers
     public class CustomersController : Controller
     {
         private ApplicationDbContext _context;
-
+        //Constructoe
         public CustomersController()
         {
             _context = new ApplicationDbContext();
@@ -21,13 +19,14 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+        //Index
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();//Eager loading Include(c => c.MembershipType)
             return View(customers);
         }
 
-
+        //Details
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);//var customer = (from cust in GetCustomers() where cust.Id == id select cust).SingleOrDefault();
@@ -36,15 +35,41 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
+        //NewCustomer
         public ActionResult NewCustomer()
         {
-
             var memberships = _context.MembershipTypes.ToList();
-            var viewModel = new CustomersViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 MembershipTypes = memberships
             };
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
+        }
+
+        //CreateCustomer
+        [HttpPost]
+        public ActionResult CreateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        //UpdateCustomer
+        public ActionResult Edit(int id)
+        {
+            var Customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (Customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                customer = Customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm",viewModel);
         }
     }
 }
---
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; } //IEnumerable because in View we only need to iterate over items not other functionalities of list like 'add'.
+        public Customer customer { get; set; }
+    }
+}
